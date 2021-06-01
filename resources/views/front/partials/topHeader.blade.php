c530376fdbeddbcf683c4912dc2944a0b44a6e59
lemonpatwari <lemonpatwari@gmail.com>
2021-06-01 18:48:26 +0600

    topheader icon change

--- resources/views/front/partials/topHeader.blade.php
+++ resources/views/front/partials/topHeader.blade.php
@@ -5,7 +5,8 @@
             <div class="col-md-4 col-sm-12">
                 <div class="header-contact">
                     <div id="info-details" class="widget-text">
-                        <i class="glyph-icon flaticon-email"></i>
+{{--                        <i class="glyph-icon flaticon-email"></i>--}}
+                        <i class="fa fa-envelope-open-o"></i>
                         <div class="info-text">
                             <a href="mailto:inf@diu-bd.net">
                                 <span>Mail Us</span>
@@ -23,7 +24,8 @@
             <div class="col-md-4 col-sm-12">
                 <div class="header-contact pull-right">
                     <div id="phone-details" class="widget-text">
-                        <i class="glyph-icon flaticon-phone-call"></i>
+{{--                        <i class="glyph-icon flaticon-phone-call"></i>--}}
+                        <i class="fa fa-phone"></i>
                         <div class="info-text">
                             <a href="tel:01611348344">
                                 <span>Call Us</span>
