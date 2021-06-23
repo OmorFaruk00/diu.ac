0da88f3f04a40e200aa84531b670d87db0cfa02f
lemonpatwari <lemonpatwari@gmail.com>
2021-06-23 18:06:10 +0600

    meta tag add

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -3,29 +3,40 @@
 <head>
     <!-- meta tag -->
     <meta charset="utf-8">
-    <title>DIU | Dhaka International University</title>
-    <meta name="description" content="">
-    <!-- responsive tag -->
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- favicon -->
+    <title>DIU | Dhaka International University</title>
+    <meta name="description"
+          content="Dhaka International University DIU is one of the leading, familiar and note-worthy private universities in Bangladesh which was established on 7th April 1995. It is a non-profitable institution having strict academic discipline">
+    <meta name="keywords"
+          content="DIU, Dhaka International University, Dhaka, Dhaka International, University, Private Universities In Bangladesh, Universities In Bangladesh">
+    <meta name="author" content="Dhaka International University">
+    <meta property="og:locale" content="en_US"/>
+    <meta property="og:type" content="website"/>
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:site" content="@diubd"/>
+    <meta name="google-site-verification" content="A8rwnYA9M2SLmwpyC8lbmsPvrY-QkJWoBb4-B0aitc4"/>
+
+    <link rel="canonical" href="{{ urldecode(url()->full()) ?? env('api_url') }}"/>
+    <meta property="og:title"
+          content="{{ \Request::route()->getName() ?? 'DIU | DIU | Dhaka International University' }}"/>
+    <meta property="og:url" content="{{ urldecode(url()->full()) ?? env('api_url') }}"/>
+
     <link rel="apple-touch-icon" href="apple-touch-icon.html">
     <link rel="shortcut icon" type="image/x-icon" href="/favicons.png">
 
-{{--    @include('front.partials.allCss')--}}
 
+    {{--@include('front.partials.allCss')--}}
     <link rel="stylesheet" type="text/css" href="{{ asset('/') }}css/all.min.css">
-
     {{-- output the compiled JS --}}
     <script src="{{ asset('js/app.js') }}" defer></script>
+    {{--@include('front.partials.allJs')--}}
+    <script src="{{ asset('/') }}js/all.min.js" defer></script>
+
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js" defer></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js" defer></script>
     <![endif]-->
-
-    {{--all javascript--}}
-    {{--@include('front.partials.allJs')--}}
-    <script src="{{ asset('/') }}js/all.min.js" defer></script>
 
     <style>
 
@@ -59,7 +70,6 @@
             border-bottom: 2px solid #FFFFFF;
         }
     </style>
-
     @stack('style')
 
 </head>
@@ -91,17 +101,18 @@
     var chatbox = document.getElementById('fb-customer-chat');
     chatbox.setAttribute("page_id", "160248167326381");
     chatbox.setAttribute("attribution", "biz_inbox");
-    window.fbAsyncInit = function() {
+    window.fbAsyncInit = function () {
         FB.init({
-            xfbml            : true,
-            version          : 'v11.0'
+            xfbml: true,
+            version: 'v11.0'
         });
     };
 
-    (function(d, s, id) {
+    (function (d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
+        js = d.createElement(s);
+        js.id = id;
         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
