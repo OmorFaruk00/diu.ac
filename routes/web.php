99ae283fa426d257f10817045dc22f6378c532a3
lemonpatwari <lemonpatwari@gmail.com>
2021-06-29 16:10:07 +0600

    artisan command update

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/cache-clear', function () {
-
-    \Artisan::call('route:clear');
-    \Artisan::call('config:clear');
     \Artisan::call('cache:clear');
-
-    \Artisan::call('route:cache');
-    \Artisan::call('config:cache');
-
     return response()->json(['message'=>'cache clear'],200);
 
 });
