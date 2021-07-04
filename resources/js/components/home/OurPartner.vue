c85351922dcd4d03f11f152d9ec9cd8f6c321b78
lemonpatwari <lemonpatwari@gmail.com>
2021-07-04 15:01:06 +0600

    partner carousel item modified

--- resources/js/components/home/OurPartner.vue
+++ resources/js/components/home/OurPartner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="rs-carousel owl-carousel" data-loop="true" data-items="5" data-margin="80" data-autoplay="true"
+    <div class="rs-carousel owl-carousel" data-loop="true" data-items="6" data-margin="80" data-autoplay="true"
          data-autoplay-timeout="2000" data-smart-speed="2000" data-dots="false" data-nav="false"
          data-nav-speed="false" data-mobile-device="2" data-mobile-device-nav="false"
          data-mobile-device-dots="false" data-ipad-device="4" data-ipad-device-nav="false"
