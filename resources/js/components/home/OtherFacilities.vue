fa0815b7c92ab5371df9f2d2b62ffc0e5a906893
lemonpatwari <lemonpatwari@gmail.com>
2021-07-06 11:27:25 +0600

    other facility component add

---
+++ resources/js/components/home/OtherFacilities.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="about-desc">
+        <h3>Other Facilities</h3>
+    </div>
+    <div id="facilitiesAccordion" class="rs-accordion-style1">
+
+        <div v-for="(otherFacility,index) in otherFacilities" :key="index" class="card">
+            <div class="card-header" :id="`otherFacility${index}`">
+                <h3 class="acdn-title collapsed" data-toggle="collapse"
+                    :data-target="`#collapseAcademicSession${index}`"
+                    aria-expanded="true" :aria-controls="`collapseAcademicSession${index}`">
+                    {{ otherFacility.title }}
+                </h3>
+            </div>
+            <div :id="`collapseAcademicSession${index}`" class="collapse" :aria-labelledby="`otherFacility${index}`"
+                 data-parent="#facilitiesAccordion">
+                <div class="card-body">
+                    {{ otherFacility.description }}
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import {otherFacilities} from "../../data/otherFacilities";
+
+export default {
+    name: "OtherFacilities",
+    data() {
+        return {
+            otherFacilities
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/data/otherFacilities.js
@@ -0,0 +1,37 @@
+export const otherFacilities = [
+    {
+        id: '1',
+        title: 'Academic Session',
+        description:"The Academic year is mainly divided into three semesters. Each semester consists of 18 weeks including examinations. Dhaka International University (DIU) has its own Holiday list.",
+    },
+
+    {
+        id: '2',
+        title: 'Health Service',
+        description:"Dhaka international University has health services department . Doctors are available in all campuses . They consult the students with their health related problems.",
+    },
+
+    {
+        id: '3',
+        title: 'Accommodation',
+        description:"DIU officers well-decorated hotels with modern facilities. At present there are six boys hostel and one girls' hostel.",
+    },
+
+    {
+        id: '4',
+        title: 'Student’s Welfare',
+        description:"Dhaka international University is committed to serve the students. As per private university act - 2010, DIU authority has appointed student welfare adviser. Student welfare adviser conselts with the students regarding their problems and solv the problems.",
+    },
+
+    {
+        id: '5',
+        title: 'Career Counseling',
+        description:"DIU has its career counseling department. An institute has been set up. The name of the instituted is Center for Excellence and Career Development. CECD organizes career related seminar, workshop and job fair for the students.",
+    },
+
+    {
+        id: '6',
+        title: 'On-Course Counseling',
+        description:"Every teachers of Dhaka international University has allocated their time in their daily class routin for on cours counciling for students. Students get benefite form ther on-cours counseling. It is compulsary for all teachers to allocate time for the students."
+    },
+];
